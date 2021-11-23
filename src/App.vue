7d6158f630e43d48c5bb314cdb16d22fd0cdda80
Katrina Curro <kcurr3@unh.newhaven.edu>
2021-11-23 11:01:35 -0600

    creating framework for front end

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,51 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<div class="container">
+  <Header city="Southbury" state="CT"/>
+  <About/>
+</div>
+
+  <!--<HelloWorld msg="Weclome to Class"/>-->
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+//import HelloWorld from './components/HelloWorld.vue'
 
+import Header from './components/Header.vue'
+
+
+//data url http://localhost:5555/books 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    //HelloWorld
+    Header
+  },
 }
 </script>
 
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+@import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@300&display=swap');
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+
+body {
+  font-family: 'Montserrat', sans-serif;
+}
+.container {
+  max-width: 400px;
+  margin: 30px auto;
+  overflow: auto;
+  min-height: 300px;
+  border: 0.3em solid black;
+  padding: 30px;
+  border-radius: 5px;
+}
+
+div{
+  margin-bottom: 0.5em;
 }
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,26 @@
+<template>
+<div>
+    <h1> {{city}} </h1>
+    <h4> {{state}} </h4>
+</div>
+</template>
+
+<script>
+
+export default{
+    name: 'Header',
+    props:{
+        city: String,
+        state: String
+    }
+}
+
+</script>
+
+<style scoped>
+
+div{
+    text-align: center;
+}
+
+</style>
